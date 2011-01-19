80d434c6b821e9ce58a74518dce47c9cfc3e734c
Kurt Harriger <kurtharriger@gmail.com>
2011-01-18 22:57:36 -0700

    decompose main logic a bit

--- program.js
+++ program.js
@@ -2,24 +2,32 @@
 var fs = require('fs');
 var assert = require('assert');
 
+
 // reduce list of key,count into map[key] = total
 var reducer = function(acc, item) {
-  
+    
 }
 
-fs.readFile('data.txt', 'utf-8', function(err, data) {
-  if(err) throw err;
-
+var reduceFileData = function(data) {    
   var lines = data.split('\n');
   console.log(lines);
 
   var result = lines.reduce(reducer, {});
   console.log(result);
-
+  
   assert.ok(result["John"] == 6, "total for John should be 6");
-  assert.ok(result["Jane"] == 8, "total for Jane should be 8");
+  assert.ok(result["Jane"] == 8, "total for Jane should be 8");  
+}
 
-  process.stdout.write("The total for John is " + result["John"] + ".");
-  process.stdout.write("The total for Jane is " + result["Jane"] + ".");
+var displaySummary = function(summary) {
+  process.stdout.write("The total for John is " + summary["John"] + ".");
+  process.stdout.write("The total for Jane is " + summary["Jane"] + ".");
+}
 
+
+fs.readFile('data.txt', 'utf-8', function(err, data) {
+  if(err) throw err;
+
+  var summary = reduceFileData(data);
+  displaySummary(summary);
 });
