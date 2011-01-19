8638984e39ec97e9808e3c11ef0048b80c23f7de
Kurt Harriger <kurtharriger@gmail.com>
2011-01-18 23:03:41 -0700

    create reducer test

--- program.js
+++ program.js
@@ -8,6 +8,18 @@ var reducer = function(acc, item) {
     
 }
 
+var testReducer = function() {
+  var lines = [];
+  lines.push("John,2");
+  lines.push("Jane,3");
+  lines.push("John,4");
+  lines.push("Jane,5");
+  var result = lines.reduce(reducer, {});
+  assert.ok(result["John"] == 6, "total for John should be 6");
+  assert.ok(result["Jane"] == 8, "total for Jane should be 8");  
+}
+
+
 var reduceFileData = function(data) {    
   var lines = data.split('\n');
   console.log(lines);
@@ -15,8 +27,7 @@ var reduceFileData = function(data) {
   var result = lines.reduce(reducer, {});
   console.log(result);
   
-  assert.ok(result["John"] == 6, "total for John should be 6");
-  assert.ok(result["Jane"] == 8, "total for Jane should be 8");  
+    
 }
 
 var displaySummary = function(summary) {
@@ -25,9 +36,13 @@ var displaySummary = function(summary) {
 }
 
 
-fs.readFile('data.txt', 'utf-8', function(err, data) {
-  if(err) throw err;
+testReducer();
 
-  var summary = reduceFileData(data);
-  displaySummary(summary);
-});
+/*
+  fs.readFile('data.txt', 'utf-8', function(err, data) {
+    if(err) throw err;
+
+    var summary = reduceFileData(data);
+    displaySummary(summary);  
+  });
+*/
